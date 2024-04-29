73c35ea0b8cadcc3cb0d5516c39dc7df2d53af7e
Sergei Zhitenev <s.zhitenev@finmars.com>
2024-04-30 00:24:20 +0200

    add card component

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,7 @@
 import '../src/assets/css/base-theme.css';
 import '../src/assets/css/base.css';
 
+
 const preview = {
   parameters: {
     controls: {
@@ -12,31 +13,46 @@ const preview = {
       },
     },
   },
+  globalTypes: {
+    theme: {
+      name: 'Theme',
+      description: 'Global theme for components',
+      defaultValue: 'light',
+      toolbar: {
+        // The label to show for this toolbar item
+        title: 'Theme',
+        icon: 'circlehollow',
+        // Array of plain string values or MenuItem shape (see below)
+        items: ['light', 'dark'],
+        // Change title based on selected value
+        dynamicTitle: true,
+      },
+    },
+  },
+  decorators: [
+    (story, context) => {
+      const theme = context.globals.theme || 'light';
+
+      if (theme === 'dark') {
+        document.body.classList.add('dark-mode');
+        document.body.classList.remove('light-mode');
+      } else {
+        document.body.classList.add('light-mode');
+        document.body.classList.remove('dark-mode');
+      }
+
+      return {
+        components: { story},
+        template: `
+          <div>
+            <story/>
+          </div>
+      `,
+      };
+    },
+  ],
 };
 
 export default preview;
 
-export const globalTypes = {
-  theme: {
-    name: 'Theme',
-    description: 'Global theme for components',
-    defaultValue: 'light',
-    toolbar: {
-      icon: 'circlehollow',
-      // Array of theme items
-      items: [
-        { value: 'light', title: 'Light', left: '☀️', right: 'Light' },
-        { value: 'dark', title: 'Dark', left: '🌙', right: 'Dark' }
-      ],
-    },
-  },
-};
 
-export const decorators = [
-  (Story, context) => {
-    const theme = context.globals.theme;
-    return {
-      template: `<div class="${theme === 'dark' ? 'dark-mode' : ''}"><story /></div>`,
-    };
-  },
-];
---
+++ src/components/finmars-card/FinmarsCardHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="finmars-card-header">
+    <slot></slot> <!-- Allow anything to be passed via slot -->
+  </div>
+</template>
+
+<script>
+// import './button.css';
+import {reactive} from 'vue';
+
+export default {
+  name: 'finmars-card-header',
+
+  props: {},
+
+  setup(props, {emit}) {
+    props = reactive(props);
+
+  },
+};
+</script>
+<style lang="scss" scoped>
+
+.finmars-card-header {
+  padding: 0.5rem;
+}
+
+</style>
