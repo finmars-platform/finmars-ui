becda6aa6c95ec18584ff285a2ee02a28d5931ee
Sergei Zhitenev <s.zhitenev@finmars.com>
2024-04-29 22:12:36 +0200

    change to finmar-ui.css

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Finmars UI
+
+
+## Table of Contents
+
+To build
+
+`npm run build`
+
+To patch (when publish new version) - run after you commit and push changes
+
+`npm version patch`
+
+To publish
+
+`npm publish`
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
     plugins: [vue()],
     build: {
+        cssCodeSplit: false, // Important: Forces all CSS into a single file
         lib: {
             entry: './index.js',
             name: 'FinmarsUI',
@@ -12,11 +13,13 @@ export default defineConfig({
         rollupOptions: {
             // Make sure external dependencies are not bundled into your library
             external: ['vue'],
+
             output: {
                 // Provide globals for external modules
                 globals: {
                     vue: 'Vue'
-                }
+                },
+                assetFileNames: `finmars-ui.css`
             }
         }
     }
