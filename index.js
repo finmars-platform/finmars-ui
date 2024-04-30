ea7dcc80dc8f7984e08b20826626fe25fee8c031
Sergei Zhitenev <s.zhitenev@finmars.com>
2024-04-30 13:45:59 +0200

    update index.js export

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 import FinmarsButton from './src/components/finmars-button/FinmarsButton.vue';
+import FinmarsCard from './src/components/finmars-card/FinmarsCard.vue';
+import FinmarsCardHeader from './src/components/finmars-card/FinmarsCardHeader.vue';
+import FinmarsCardContent from './src/components/finmars-card/FinmarsCardContent.vue';
+import FinmarsCardFooter from './src/components/finmars-card/FinmarsCardFooter.vue';
 
-export { FinmarsButton };
+export { FinmarsButton, FinmarsCard, FinmarsCardHeader, FinmarsCardContent, FinmarsCardFooter}
