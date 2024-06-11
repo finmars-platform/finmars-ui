7a10e3a4fc34cd4900329b7933101fce0ea370d1
Hrach <h.karapetyand5@gmail.com>
2024-06-11 11:45:02 +0400

    added dark/light mode in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,38 @@
 <template>
-
+    <div class="flex justify-end m-2">
+        <fm-btn
+            @click="toggleDarkMode"
+            type="filled"
+        >
+            {{ toggleText }}
+        </fm-btn>
+    </div>
 </template>
 
 <script setup>
+import {computed, onMounted, ref} from 'vue';
+import FmBtn from './components/fm/Btn.vue'
 
+const darkMode = ref(false);
+
+function updateBodyClass(isDark) {
+    if (isDark) {
+        document.body.classList.add('dark');
+    } else {
+        document.body.classList.remove('dark');
+    }
+}
+
+const toggleText = computed(() => !darkMode.value ? 'Enable Dark Theme' : 'Disable Dark Theme');
+
+function toggleDarkMode() {
+    darkMode.value = !darkMode.value;
+    updateBodyClass(darkMode.value);
+    sessionStorage.setItem('darkMode', darkMode.value ? 'true' : 'false');
+}
+
+onMounted(() => {
+    darkMode.value = sessionStorage.getItem('darkMode') === 'true';
+    updateBodyClass(darkMode.value);
+});
 </script>
