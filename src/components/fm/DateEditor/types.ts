e81a7d94170358b78dae6919f2077f5ee8b06a52
Vitali Kashkel <v.kashkel@finmars.com>
2025-01-24 10:43:28 +0000

    FIX: fix DateEditor

--- src/components/fm/DateEditor/types.ts
+++ src/components/fm/DateEditor/types.ts
@@ -1,28 +1,28 @@
 export interface FmDateEditorProps {
-	modelValue: string;
-	min?: string; // data in ISO 8601 format (YYYY-MM-DD)
-	max?: string; // data in ISO 8601 format (YYYY-MM-DD)
-	nonWorkingDays?: string[];
-	erroneousDates?: string[];
-	allowWeekendSelection?: boolean;
-	showAdjacentMonths?: boolean;
-	calculatePreviousDayFromToday?: boolean;
-	includeTime?: boolean;
-	simple?: boolean;
-	disabled?: boolean;
-	locals?: {
-		enteringFieldLabel: string;
-		enteringFieldPlaceholder: string;
-		cancelButtonText: string;
-		confirmButtonText: string;
-		menuItemCustom: string;
-		menuPresetToday: string;
-		menuPresetPrevious: string;
-	};
+  modelValue?: string;
+  min?: string; // data in ISO 8601 format (YYYY-MM-DD)
+  max?: string; // data in ISO 8601 format (YYYY-MM-DD)
+  nonWorkingDays?: string[];
+  erroneousDates?: string[];
+  allowWeekendSelection?: boolean;
+  showAdjacentMonths?: boolean;
+  calculatePreviousDayFromToday?: boolean;
+  includeTime?: boolean;
+  simple?: boolean;
+  disabled?: boolean;
+  locals?: {
+    enteringFieldLabel: string;
+    enteringFieldPlaceholder: string;
+    cancelButtonText: string;
+    confirmButtonText: string;
+    menuItemCustom: string;
+    menuPresetToday: string;
+    menuPresetPrevious: string;
+  };
 }
 
 export interface FmDateEditorEmits {
-	(event: 'update:modelValue', value: string): void;
-	(event: 'click:dateIcon'): void;
-	(event: 'cancel'): void;
+  (event: 'update:modelValue', value: string): void;
+  (event: 'click:dateIcon'): void;
+  (event: 'cancel'): void;
 }
